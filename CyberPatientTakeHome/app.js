7cd9880862dfa1b0b640bb71e8825718672b7cb5
Bernardo <bernardoasc@gmail.com>
2021-10-03 12:12:55 -0700

    combined category and name search
    name and category can be used in one form + added code formatting & comments for better readability

--- CyberPatientTakeHome/app.js
+++ CyberPatientTakeHome/app.js
@@ -1,92 +1,129 @@
-
-// Import Files and Libraries
+//dependencies imported
 const port = 3000;
-const express= require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
-const app= express();
+const app = express();
 const data = require("./medications.json");
 
-// define location of static files
-app.use(bodyParser.urlencoded({extended: false}));
+//locating files
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
 
+// initializing arrays
 var categories = [];
 var displays = [];
 
+// setting category array (sets all unique categories)
 data.forEach(medication => {
     var cat = medication.category;
     var exists = false;
 
-    if(categories.length == 0) categories.push(cat);
-    else{
-        for(var i = 1; i <= categories.length; i++){
-            if(cat == categories[i - 1]) {
+    if (categories.length == 0) categories.push(cat);
+    else {
+        for (var i = 1; i <= categories.length; i++) {
+            if (cat == categories[i - 1]) {
                 exists == true;
                 return;
             }
             else exists == false;
         }
-        if( exists == false) categories.push(cat);
+        if (exists == false) categories.push(cat);
     }
-    
+
 });
 
-
-
-
-// set the get and post routes and request and response
+// setting get and post routes
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/public/views'))
 
 app.get('/', (req, res) => {
+    //makes sure display array is empty
     displays = [];
+    // fills array with all items (loading list with no search items)
     data.forEach(medication => {
         displays.push(medication);
     })
-    res.render('index', {displays, categories});
+    res.render('index', { displays, categories });
 });
 
-app.post("/index", (req,res)=>{
-    
-    searchName=req.body.name;
-    
-    res.redirect("/name");
+app.post("/", (req, res) => {
+
+    //grabs both select and text box inputs
+    searchName = req.body.name;
+    searchCat = req.body.category;
+
+    res.redirect("/search");
 });
 
-app.get("/name", (req, res)=>{
+app.get("/search", (req, res) => {
+
+    //makes sure array is empty
     displays = [];
-    reg = new RegExp(searchName, 'i');
+
+    // forcing input to be a string (not null)
+    var cat = searchCat + "";
+    var name = searchName + "";
+
+    // if category has a value other than default
+    if (cat !== "") {
+        data.forEach(medication => {
+
+            //starts checking only medications within category
+            if (cat == medication.category) {
+
+                //checks if name is a value other than default
+                if (name !== "") {
+
+                    // sets regExp for substrings
+                    reg = new RegExp(name, 'i');
+
+                    //checks if any medication name has a substring of name input
+                    if (reg.test(medication.name)) {
+
+                        //adds that medication to array
+                        displays.push(medication);
+
+                    }
+                }
+                // if name value is default, display all medication with same category
+                else {
+                    displays.push(medication);
+                }
+            }
+
+        })
+    }
+    //if category is set to default value
+    else {
+
+        if (name !== "") {
+            data.forEach(medication => {
+                reg = new RegExp(name, 'i');
+                if (reg.test(medication.name))
+                    displays.push(medication);
+            });
+        }
+        // if both category and name are defaul, display full list
+        else {
+            data.forEach(medication => {
+                displays.push(medication);
+            });
+        }
+    }
+
     data.forEach(medication => {
-        if(reg.test(medication.name))
-        displays.push(medication);
+
     })
-    res.render('index', {displays, categories});
+    // rendex index page with new medications
+    res.render('index', { displays, categories });
 })
 
-app.post("/", (req,res)=>{
-    searchCat=req.body.category;
-    res.redirect("/category");
-});
 
-app.get('/category', (req, res) => {
-    displays = [];
-
-    data.forEach(medication => {
-        if(searchCat == medication.category)
-        displays.push(medication);
-    })
-
-    res.render('index', {displays, categories});
-});
-
-
-
-// set up the server, log the start
-
+//sets up the server
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+    console.log('The application is running on localhost:3000')
 });
